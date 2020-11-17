5d656a7b751368d3102d7ba8a870b3b28b4b8f33
AtteMa <martikainen.atte@gmail.com>
2020-11-17 21:40:54 +0200

    kirjan ja linkin lisääminen

--- src/main/java/Scrumbags/ui/Ui.java
+++ src/main/java/Scrumbags/ui/Ui.java
@@ -2,6 +2,8 @@ package Scrumbags.ui;
 
 // Tästä pitänee tehdä injektio
 import java.util.Scanner;
+import Scrumbags.database.*;
+import Scrumbags.logic.*;
 
     /**
      * Lukuvinkkipalvelun tekstipohjainen käyttöliittymä
@@ -9,6 +11,9 @@ import java.util.Scanner;
 public class Ui {
     
     private IO io;
+    private Service service;
+    private String[] str;
+    private DatabaseFake database = new DatabaseFake();
     
      /**
      * Käyttöliittymän parametriton konstruktori,joka luo käyttjän syötteitä
@@ -16,6 +21,7 @@ public class Ui {
      */
     public Ui(){
         this.io = new KonsoliIO();
+        this.service = new Service(database);
     }
     
     /**
@@ -23,15 +29,29 @@ public class Ui {
      */
     public void run(boolean go){
         String komento = "";
+        System.out.println("Tervetuloa!");
         while(go){
-            
-            System.out.println("Anna komento (q tai add)");
+            System.out.println("\nkomennot:\n"
+                            +"q\n"
+                            +"add book\n"
+                            +"add link\n\n"
+                            +"Anna komento:");
             komento = io.nextLine();
-            
+            /**
+             * lukuvinkkien lisääminen käyttäjän syötteestä
+             */
             if(komento.equals("q")){
                 go = false;
-            } else if(komento.equals("add")){
-                System.out.println("Lisäys");
+            } else if(komento.equals("add book")){
+                System.out.println("Anna kirjan nimi ja kirjailija muodossa 'nimi/kirjailija'");
+                komento = io.nextLine();
+                str = komento.split("/");
+                service.addBook(str[0], str[1]);
+            } else if(komento.equals("add link")){
+                System.out.println("Anna linkin nimi ja URL-osoite muodossa 'nimi/URL'");
+                komento = io.nextLine();
+                str = komento.split("/");
+                service.addLink(str[0], str[1]);
             }
         }
         
