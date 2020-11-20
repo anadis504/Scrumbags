f73c1b5087b1616e44cd4da242e9a841a6965ef4
Rasmus <rasmus.marttila@hotmail.com>
2020-11-20 12:26:54 +0200

    Lisätty year (taas)

--- src/main/java/Scrumbags/logic/Book.java
+++ src/main/java/Scrumbags/logic/Book.java
@@ -6,6 +6,7 @@ public class Book {
     String author;
     String isbn;
     int pages;
+    int year;
     
     public Book(String name, String author) {
         this.name = name;
@@ -34,6 +35,14 @@ public class Book {
 
     public int getPages() {
         return pages;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     @Override
