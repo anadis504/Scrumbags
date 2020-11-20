a084602cae231fe7fe244827f88cbbd358517c48
Toni Rämö <toni.ramo@helsinki.fi>
2020-11-20 12:54:24 +0200

    add first stepsdefs

--- src/test/java/Scrumbags/Stepdefs.java
+++ src/test/java/Scrumbags/Stepdefs.java
@@ -5,10 +5,31 @@
  */
 package Scrumbags;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import java.util.List;
+
 /**
  *
  * @author toniramo
  */
 public class Stepdefs {
+    List<String> lines;
     
+    @Given("command add book is selected")
+    public void commandAddBookSelected() {
+        lines.add("add book");
+    }
+    
+    @When("valid book name {string} and valid writer name {string} are entered")
+    public void validBooknameAndValidWriternameEntered(String book, String writer) {
+        lines.add(book);
+        lines.add(writer);
+        
+        //run ui;
+    }
+    
+    @Then("new bookmark for a book is created")
 }
