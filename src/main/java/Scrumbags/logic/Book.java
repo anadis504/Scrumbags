68331bb0ecd1974f750afb6b74be9ccf5d564808
Rasmus <rasmus.marttila@hotmail.com>
2020-11-20 12:15:19 +0200

    Lisätty kirjalle julkaisuvuosi

--- src/main/java/Scrumbags/logic/Book.java
+++ src/main/java/Scrumbags/logic/Book.java
@@ -6,6 +6,7 @@ public class Book {
     String author;
     String isbn;
     int pages;
+    int year;
     
     public Book(String name, String author) {
         this.name = name;
@@ -36,10 +37,16 @@ public class Book {
         return pages;
     }
 
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
     @Override
     public String toString() {
         return "Nimi: "+getName()+"\nKirjailija: "+getAuthor()+"\nISBN: "+getIsbn()+"\nSivumäärä: "+getPages();
     }
-    
-    
 }
