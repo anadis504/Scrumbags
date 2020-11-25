2de4c82c1f96587562f8c9c22c25035d37bf63be
Sanna <sanna.korpi@live.fi>
2020-11-25 18:13:16 +0200

    service luokan kirjan tarkistus ennen lisäystä

--- src/main/java/Scrumbags/logic/Service.java
+++ src/main/java/Scrumbags/logic/Service.java
@@ -11,12 +11,16 @@ public class Service {
         this.database = database;
     }
     
+    // Lisättäessä nimellä ja kirjailijalla, tarkistaa onko samannimistä kirjaa olemassa jolla on isbn.
+    // Ei hyväksytä duplaria jos saman niminen isbn:llä varustettu löytyy
     public boolean addBook(String name, String author) {
         Book book = new Book(name, author);
+        if (bookNameExists(name) && !getBookByName(name).getIsbn().equals("---")) return false;
         return this.database.addBook(book);
     }
     public boolean addBook(String name, String author, String isbn, int pages, int  year) {
         Book book = new Book(name, author, isbn, pages, year);
+        if (bookIsbnExists(isbn)) return false;
         return this.database.addBook(book);
     }
     
@@ -27,5 +31,17 @@ public class Service {
 
     public ArrayList<Book> getBooksByAuthor(String author) {
         return this.database.getBooksByAuthor(author);
+    }
+    
+    public Book getBookByName(String name) {
+        return database.getBookByName(name);
+    }
+    
+    public boolean bookIsbnExists(String isbn) {
+        return database.getBookByIsbn(isbn) != null;
+    }
+    
+    public boolean bookNameExists(String isbn) {
+        return database.getBookByIsbn(isbn) != null;
     }
 }
