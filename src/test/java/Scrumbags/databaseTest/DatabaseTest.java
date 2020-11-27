79819512911b3feb7ad64afd594a97909a49c39a
Toni Rämö <toni.ramo@helsinki.fi>
2020-11-27 10:59:02 +0200

    Increase database test coverage

--- src/test/java/Scrumbags/databaseTest/DatabaseTest.java
+++ src/test/java/Scrumbags/databaseTest/DatabaseTest.java
@@ -8,6 +8,7 @@ package Scrumbags.databaseTest;
 import Scrumbags.database.*;
 import Scrumbags.logic.*;
 import java.io.File;
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -30,6 +31,7 @@ public class DatabaseTest {
     private Book pizze = new Book("Pizze", "Saku Tuominen", "978-951-1-29097-1", 62, 2015);
 
     private Link tira = new Link("Tietorakenteet ja algoritmit", "https://tira-s19.mooc.fi/");
+    private Link ohtu = new Link("Ohjelmistotuotanto", "https://ohjelmistotuotanto-hy.github.io/");
 
     @Before
     public void setUp() throws ClassNotFoundException {
@@ -90,9 +92,31 @@ public class DatabaseTest {
     public void bookCannotBeFoundByISBNBeforeItIsAdded() {
         assertEquals(null, db.getBookByIsbn("978-951-1-36427-6"));
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+
+    @Test
+    public void addingExistingBookWithSameISBNReturnsFalse() {
+        assertFalse(db.addBook(joulupukinLomakirja));
+    }
+
+    @Test
+    public void addingExistingBookWithoutISBNReturnsTrue() {
+        pizze.setIsbn(null);
+        assertTrue(db.addBook(pizze));
+        assertTrue(db.addBook(pizze));
+    }
+
+    @Test
+    public void nonExistingBookCannotBeFoundByName() {
+        assertFalse(db.getBooksByName(pizze.getName()).contains(pizze));
+    }
+
+    @Test
+    public void nonExistingBookCannotBeFoundByYear() {
+        assertFalse(db.getBooksByYear(pizze.getYear()).contains(pizze));
+    }
+    
+    @Test
+    public void nonExistingLinkCannotBeFoundByName() {
+        assertEquals(null, db.getLinksByName(ohtu.getName()));
+    }
 }
