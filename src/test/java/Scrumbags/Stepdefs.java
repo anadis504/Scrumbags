d9b3fd3bc196160cc8d3c6a4474844595cf89e07
Rasmus <rasmus.marttila@hotmail.com>
2020-11-30 00:51:56 +0200

    testit sille, että sama isbn tai sama nimi ei voi toistua. Jostain syystä eivät mene läpi

--- src/test/java/Scrumbags/Stepdefs.java
+++ src/test/java/Scrumbags/Stepdefs.java
@@ -24,22 +24,23 @@ import Scrumbags.logic.*;
  * @author Scrumbags
  */
 public class Stepdefs {
+
     Ui ui;
     StubIO io;
     Dao dao;
     Service service;
     List<String> input;
     String testDatabaseName = "test.db";
-    
+
     @Before
-    public void setup() throws ClassNotFoundException{
-        dao = new Database("jdbc:sqlite:"+testDatabaseName);
+    public void setup() throws ClassNotFoundException {
+        dao = new Database("jdbc:sqlite:" + testDatabaseName);
         input = new ArrayList<>();
         service = new Service(dao);
         Book bonanza = new Book("Bonanza", "David R Greenland", "9781593935412", 170, 2015);
         dao.addBook(bonanza);
     }
-    
+
     @After
     public void tearDown() {
         try {
@@ -48,22 +49,19 @@ public class Stepdefs {
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
-    }    
-    
-    
-    
-    
+    }
+
     @Given("command search is selected")
     public void commandSearchIsSelected() {
         input.add("3");
     }
-    
+
     @Given("command search book by isbn is selected")
     public void commandSearchBookByIsbnIsSelected() {
         input.add("1");
         input.add("4");
     }
-    
+
     @When("a valid isbn {string} is entered")
     public void validIsbnIsEntered(String isbn) {
         input.add(isbn);
@@ -73,19 +71,20 @@ public class Stepdefs {
         ui = new Ui(io, service);
         ui.run(true);
     }
-    
+
     // Tämän testin voisi muotoilla paremmin .feature -tiedostoon että missä
     // muodossa kirjan tietojen tuloste tarkistetaan
     @Then("bookmark details {string} are shown in terminal")
     public void bookmarkDetailsAreShown(String string) {
         boolean found = false;
         for (String line : io.getOutput()) {
-            if (line.contains(string)) found = true;
+            if (line.contains(string)) {
+                found = true;
+            }
         }
         assertTrue(found);
     }
-    
-        
+
     @When("an invalid isbn {string} is entered")
     public void inValidIsbnIsEntered(String isbn) {
         input.add(isbn);
@@ -95,18 +94,18 @@ public class Stepdefs {
         ui = new Ui(io, service);
         ui.run(true);
     }
-    
+
     @Then("search has no results")
     public void searchHasNoResults() {
         assertTrue(io.getOutput().contains("Ei tuloksia."));
     }
-    
+
     @Given("command search book by year is selected")
     public void commandSearchBookByYearIsSelected() {
         input.add("1");
         input.add("3");
     }
-    
+
     @When("a non-existing year {string} is entered")
     public void nonExistingYearIsEntered(String year) {
         input.add(year);
@@ -116,7 +115,7 @@ public class Stepdefs {
         ui = new Ui(io, service);
         ui.run(true);
     }
-    
+
     @When("a valid year {string} is entered")
     public void validYearIsEntered(String year) {
         input.add(year);
@@ -126,12 +125,12 @@ public class Stepdefs {
         ui = new Ui(io, service);
         ui.run(true);
     }
-    
+
     @Given("command add book is selected")
     public void commandAddBookSelected() {
         input.add("1");
     }
-    
+
     @When("valid book name {string}, writer name {string}. ISBN {string}, number of pages {string} and publication year {string} are entered and input is confirmed")
     public void validBookAttributesEnteredAndConfirmed(String book, String writer, String isbn, String pages, String year) {
         input.add(book);
@@ -141,37 +140,76 @@ public class Stepdefs {
         input.add(year);
         input.add("k");
         input.add("q");
-        
+
         io = new StubIO(input);
         ui = new Ui(io, service);
         ui.run(true);
     }
-    
+
     @Then("new bookmark for a book is created")
     public void bookmarkForBookCreated() {
         assertTrue(io.getOutput().contains("Kirja lisätty onnistuneesti."));
     }
+
+    @When("two books with the same name {string} are entered")
+    public void twoBooksWithTheSameNameAreEntered(String name) {
+        input.add("Bonanza");
+        input.add("q");
+        input.add("q");
+        input.add("q");
+        input.add("q");
+        input.add("k");
+        input.add("q");
+
+        io = new StubIO(input);
+        ui = new Ui(io, service);
+        ui.run(true);
+    }
+
+    @Then("the book is not added because name is taken")
+    public void theBookIsNotAddedBecauseNameIsTaken() {
+        assertTrue(io.getOutput().contains("Kirjaa ei onnistuttu lisäämään."));
+    }
     
+    @When("two books with the same isbn {string} are entered")
+    public void twoBooksWithTheSameIsbnAreEntered(String isbn) {
+        input.add("Bonanza2");
+        input.add(isbn);
+        input.add("q");
+        input.add("q");
+        input.add("q");
+        input.add("k");
+        input.add("q");
+
+        io = new StubIO(input);
+        ui = new Ui(io, service);
+        ui.run(true);
+    }
+
+    @Then("the book is not added because isbn is taken")
+    public void theBookIsNotAddedBecauseIsbnIsTaken() {
+        assertTrue(io.getOutput().contains("Kirjaa ei onnistuttu lisäämään."));
+    }
+
     @Given("command add link is selected")
     public void commandAddLinkSelected() {
         input.add("2");
     }
-    
+
     @When("valid link name {string} and url {string} are entered and input is confirmed")
     public void validLinkAttributesEnteredAndInputConfirmed(String name, String url) {
         input.add(name);
         input.add(url);
         input.add("k");
         input.add("q");
-        
+
         io = new StubIO(input);
         ui = new Ui(io, service);
         ui.run(true);
     }
-    
+
     @Then("new bookmark for a link is created")
     public void bookmarkForLinkCreated() {
         assertTrue(io.getOutput().contains("Linkki lisätty onnistuneesti."));
     }
-    
 }
--- src/test/resources/Scrumbags/new_bookmark.feature
+++ src/test/resources/Scrumbags/new_bookmark.feature
@@ -5,6 +5,16 @@ Feature: User can create new bookmark
         When  valid book name "Story of Scrumbags", writer name "Test Writer". ISBN "123-45-67890-12-3", number of pages "123" and publication year "2020" are entered and input is confirmed
         Then  new bookmark for a book is created
 
+    Scenario: user can't add two books with the same name
+        Given command add book is selected
+        When two books with the same name "Bonanza" are entered
+        Then the book is not added because name is taken
+
+    Scenario: user can't add two books with the same isbn
+        Given command add book is selected
+        When two books with the same isbn "9781593935412" are entered
+        Then the book is not added because name is taken
+
     Scenario: user can create new bookmark for link
         Given command add link is selected
         When  valid link name "Ohtu2020" and url "https://ohjelmistotuotanto-hy.github.io/" are entered and input is confirmed
