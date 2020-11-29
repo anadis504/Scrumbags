48edf8deff17288e9562db62a00cdc56bfd2e5fc
Rasmus <rasmus.marttila@hotmail.com>
2020-11-30 01:30:58 +0200

    Testitiedostojen refaktorointi

--- src/test/java/Scrumbags/logicTest/LisaysTest.java
+++ src/test/java/Scrumbags/logicTest/LisaysTest.java
@@ -46,84 +46,10 @@ public class LisaysTest {
         }
         assertEquals(true, found);
     }
-    
-    @Test
-    public void searchReturnsRightBook() {
-        this.service.addBook("ABC", "Taavi", "123-14", 50, 2005);
-        ArrayList<Book> booklist = this.service.getBooksByAuthor("Taavi");
-        boolean found = false;
-        for (Book b: booklist) {
-            if (b.getName().equals("ABC") && b.getIsbn().equals("123-14") && b.getPages() == 50 && b.getYear() == 2005) {
-                found = true;
-            }
-        }
-        assertEquals(true, found);
-    }
-
-    @Test
-    public void bookAmountIsRight() {
-        this.service.addBook("Aapinen", "Nobody");
-        this.service.addBook("Bk", "Nobody");
-        this.service.addBook("MAGA", "Nobody");
-        assertEquals(3, this.service.getBooksByAuthor("Nobody").size());
-    }
 
     @Test
     public void noDuplicateBooksAdded() {
         this.service.addBook("nimi", "kirjailija", "isbn1234", 123, 123);
         assertFalse(this.service.addBook("1", "2", "isbn1234", 3, 4));
     }
-
-    @Test
-    public void isbnSearchReturnsRightBook() {
-        this.service.addBook("ASD", "WASD", "123-14", 50, 13);
-        ArrayList<Book> booklist = this.service.getBookByIsbn("123-14");
-        boolean found = false;
-        for (Book b: booklist) {
-            if (b.getName().equals("ASD") && b.getIsbn().equals("123-14") && b.getPages() == 50 && b.getYear() == 13) {
-                found = true;
-            }
-        }
-        assertEquals(true, found);
-    }
-
-    @Test
-    public void isbnSearchFailsIfBookNotAddedYet() {
-        assertNull(this.service.getBookByIsbn("123-14"));
-    }
-    
-    
-    @Test
-    public void yearSearchReturnsRightBook() {
-        this.service.addBook("ABC", "Taavi", "123-14", 50, 2005);
-        ArrayList<Book> booklist = this.service.getBooksByYear(2005);
-        boolean found = false;
-        for (Book b: booklist) {
-            if (b.getName().equals("ABC") && b.getIsbn().equals("123-14") && b.getPages() == 50 && b.getYear() == 2005) {
-                found = true;
-            }
-        }
-        assertEquals(true, found);
-    }
-
-    @Test
-    public void yearSearchFailsIfBookNotAddedYet() {
-        assertNull(this.service.getBooksByYear(1044));
-    }
-    
-    @Test
-    public void yearSearchFailsIfYearDoesNotExistsInAnyBook() {
-        this.service.addBook("ABC", "Taavi", "123-14", 50, 2005);
-        this.service.addBook("nimi", "kirjailija", "isbn1234", 123, 123);
-        assertNull(this.service.getBooksByYear(2000));
-    }
-    
-//    @Test
-//    public void bookmarkAmountIsRight() {
-//        this.service.addBook("Aapinen", "Tuntematon");
-//        this.service.addLink("Google", "http://www.google.com");
-//        this.service.addLink("Omasivu", "http://www.urpo.fi");
-//        assertEquals(this.service.getBookList().size()+this.service.getLinkList().size() == 3);
-//    }
-    
 }
