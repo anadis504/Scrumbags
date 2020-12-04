9ec1553dfef0d3b711e669d9669cd092425d47d8
Toni Rämö <toni.ramo@helsinki.fi>
2020-12-04 14:56:59 +0200

    Lisää Database-testit kaikkien kirjamerkkien listaukselle

--- src/test/java/Scrumbags/databaseTest/DatabaseTest.java
+++ src/test/java/Scrumbags/databaseTest/DatabaseTest.java
@@ -35,11 +35,15 @@ public class DatabaseTest {
     private Link tira = new Link("Tietorakenteet ja algoritmit", "https://tira-s19.mooc.fi/");
     private Link ohtu = new Link("Ohjelmistotuotanto", "https://ohjelmistotuotanto-hy.github.io/");
 
+    private Podcast bbc = new Podcast("BBC Newscast", "BBC", "https://www.bbc.co.uk/programmes/p05299nl/episodes/downloads", "https://podcasts.files.bbci.co.uk/p05299nl.rss");
+    private Podcast pmh = new Podcast("Pieleen mennyt historia", "Yle", "https://areena.yle.fi/audio/1-50677839", "https://feeds.yle.fi/areena/v1/series/1-50677839.rss?lang=fi&downloadable=true");
+
     @Before
     public void setUp() throws ClassNotFoundException {
         db = new Database("jdbc:sqlite:" + testDbName);
         db.addBook(joulupukinLomakirja);
         db.addLink(tira);
+        db.addPodcast(bbc);
     }
 
     @After
@@ -122,12 +126,12 @@ public class DatabaseTest {
     public void nonExistingLinkCannotBeFoundByName() {
         assertEquals(null, db.getLinksByName(ohtu.getName()));
     }
-    
+
     @Test
     public void removingExistingBookReturnsTrue() {
         assertTrue(db.removeBook(joulupukinLomakirja.getIsbn(), joulupukinLomakirja.getName()));
     }
-    
+
     @Test
     public void removingExistingLinkReturnsTrue() {
         assertTrue(db.removeLink(tira.getAddress()));
@@ -156,24 +160,84 @@ public class DatabaseTest {
         db.removeLink(tira.getAddress());
         assertNull(db.getLinksByName(tira.getName()));
     }
-    
+
     @Test
     public void afterAddingTwoBooksWithoutISBNThoseCanBeAddedAndFoundByName() {
         db.addBook(tirakirja);
         db.addBook(scrumbook);
-        
+
         assertTrue(db.getBooksByName(tirakirja.getName()).contains(tirakirja));
         assertTrue(db.getBooksByName(scrumbook.getName()).contains(scrumbook));
     }
-    
+
     @Test
     public void afterAddingTwoBooksWithoutISBNAndRemovingOtherItCannotBeFoundByName() {
         db.addBook(tirakirja);
         db.addBook(scrumbook);
-        
+
         db.removeBook(tirakirja.getIsbn(), tirakirja.getName());
-        
+
         assertNull(db.getBooksByName(tirakirja.getName()));
         assertTrue(db.getBooksByName(scrumbook.getName()).contains(scrumbook));
     }
+
+    @Test
+    public void getAllBooksReturnsCorrectAmountOfBooks() {
+        assertTrue(db.getAllBooks().size() == 1);
+        db.addBook(pizze);
+        assertTrue(db.getAllBooks().size() == 2);
+        db.addBook(kuinkaPuutKasvavat);
+        assertTrue(db.getAllBooks().size() == 3);
+    }
+
+    @Test
+    public void getAllBooksReturnsNullIfListIsEmpty() {
+        db.removeBook(joulupukinLomakirja.getIsbn(), joulupukinLomakirja.getName());
+        assertNull(db.getAllBooks());
+    }
+
+    @Test
+    public void getAllBooksReturnsOnlyExistingBooks() {
+        assertTrue(db.getAllBooks().contains(joulupukinLomakirja));
+        assertFalse(db.getAllBooks().contains(pizze));
+    }
+
+    @Test
+    public void getAllLinksReturnsCorrectAmountOfLinks() {
+        assertTrue(db.getAllLinks().size() == 1);
+        db.addLink(ohtu);
+        assertTrue(db.getAllLinks().size() == 2);
+    }
+
+    @Test
+    public void getAllLinksReturnsNullIfListIsEmpty() {
+        db.removeLink(tira.getAddress());
+        assertNull(db.getAllLinks());
+    }
+
+    @Test
+    public void getAllLinksReturnsOnlyExistingLinks() {
+        assertTrue(db.getAllLinks().contains(tira));
+        assertFalse(db.getAllLinks().contains(ohtu));
+    }
+
+    @Test
+    public void getAllPodcastsReturnsOnlyExistingPodcasts() {
+        assertTrue(db.getAllPodcasts().contains(bbc));
+        assertFalse(db.getAllPodcasts().contains(pmh));
+    }
+
+    @Test
+    public void getAllPodcastsReturnsCorrectAmountOfPodcasts() {
+        assertTrue(db.getAllPodcasts().size() == 1);
+        db.addPodcast(pmh);
+        assertTrue(db.getAllPodcasts().size() == 2);
+    }
+
+    @Test
+    public void getAllPodcastsReturnsNullIfListIsEmpty() {
+        db.removePodcast(bbc.getName());
+        assertNull(db.getAllPodcasts());
+    }
+
 }
